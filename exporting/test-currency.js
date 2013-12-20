c359bcdd7626e9323385e98f48534357ef38c483
Bret Comnes <bcomnes@gmail.com>
2013-12-19 23:00:22 -0800

    Returned function instead of object

--- exporting/test-currency.js
+++ exporting/test-currency.js
@@ -1,7 +1,4 @@
-var currency = require('./currency');
-
-console.log('50 Canadian dollars equals this ammount of US dollars:');
+var Currency = require('./currency');
+var canadianDollar = 0.91;
+var currency = new Currency(canadianDollar);
 console.log(currency.canadianToUS(50));
-
-console.log('30 US dollars equals this amount of Canadian dollars:');
-console.log(currency.USToCanadian(30));
