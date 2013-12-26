9454c83d2250259d76a24b89b22e2a4a742e6954
Bret Comnes <bcomnes@gmail.com>
2013-12-26 14:06:02 -0800

    Started simple http todo app

---
+++ httpServers/resourcePost.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var server = http.createServer(function (req, res) {
+  req.on('data', function (chunk){
+    console.log('parsed', chunk);
+  });
+  req.on('end', function (){
+    console.log('done parsing');
+    res.end();
+  });
+});
+
+server.listen(3000);
