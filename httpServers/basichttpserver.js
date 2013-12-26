750f5067312774d59eea0ab109b6ea3b4a27aeed
Bret Comnes <bcomnes@gmail.com>
2013-12-26 13:39:59 -0800

    Set the content type

--- httpServers/basichttpserver.js
+++ httpServers/basichttpserver.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
-  res.write('Hello world.');
-  res.end();
+  var body = 'Hello world';
+  res.setHeader('Content-Length', body.length);
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(body);
 });
 server.listen(3000);
