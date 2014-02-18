955622f4b3f4edd39061a5d49412b140ca8973f7
Bret Comnes <bcomnes@gmail.com>
2014-02-18 13:39:50 -0800

    Switched to user middleware

--- shoutbox/app.js
+++ shoutbox/app.js
@@ -5,11 +5,12 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
+var user = require('./lib/middleware/user');
 var http = require('http');
 var path = require('path');
 var register = require('./routes/register');
 var messages = require('./lib/messages');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -24,7 +25,9 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(user);
 app.use(messages);
+app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {
@@ -32,9 +35,11 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 app.get('/register', register.form);
 app.post('/register', register.submit);
+app.get('/login', login.form);
+app.post('/login', login.submit);
+app.get('/logout', login.logout);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
